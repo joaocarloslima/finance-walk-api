8e0d5fba11d84ad88a2305cba8ae61dcdb12ac63
joaocarloslima <joaocarloslima@me.com>
2025-04-08 20:43:57 -0300

    add cache and swagger

--- src/main/java/br/com/fiap/finance_walk_api/App.java
+++ src/main/java/br/com/fiap/finance_walk_api/App.java
@@ -2,8 +2,15 @@ package br.com.fiap.finance_walk_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@EnableCaching
+@OpenAPIDefinition(info = @Info(title = "API Finance Walk", version = "v1", description = "API do SaaS Finance Walk de controle financeiro", contact = @Contact(name = "João Lima", email = "joao@fiap.com.br")))
 public class App {
 
 	public static void main(String[] args) {
