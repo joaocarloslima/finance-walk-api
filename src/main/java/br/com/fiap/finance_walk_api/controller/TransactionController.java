cebfae9a315da139835e81b4b812ed45716ec691
joaocarloslima <joaocarloslima@me.com>
2025-04-15 20:52:22 -0300

    add specification and pagination

--- src/main/java/br/com/fiap/finance_walk_api/controller/TransactionController.java
+++ src/main/java/br/com/fiap/finance_walk_api/controller/TransactionController.java
@@ -1,20 +1,18 @@
 package br.com.fiap.finance_walk_api.controller;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
-import org.springframework.data.domain.ExampleMatcher.StringMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.finance_walk_api.model.Transaction;
+import br.com.fiap.finance_walk_api.model.TransactionFilter;
 import br.com.fiap.finance_walk_api.repository.TransactionRepository;
+import br.com.fiap.finance_walk_api.specification.TransactionSpecification;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -22,29 +20,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class TransactionController {
 
-    record TransactionFilter(String description, LocalDate date, BigDecimal amount){}
-
     @Autowired
     private TransactionRepository repository;
 
     @GetMapping
-    public List<Transaction> index(TransactionFilter filter){
-        log.info("Buscando transações com descrição {} e data {}", filter.description(), filter.date());
-
-        var probe = Transaction.builder()
-                        .description(filter.description)
-                        .date(filter.date())
-                        .amount(filter.amount())
-                        .build();
-
-        var matcher = ExampleMatcher.matchingAll()
-                        .withIgnoreCase()
-                        .withIgnoreNullValues()
-                        .withStringMatcher(StringMatcher.CONTAINING);
-
-       var example = Example.of(probe, matcher);
-
-        return repository.findAll(example);
+    public Page<Transaction> index(TransactionFilter filter,
+            @PageableDefault(size = 10, sort = "date", direction = Direction.DESC) Pageable pageable) {
+        return repository.findAll(TransactionSpecification.withFilters(filter), pageable);
     }
-    
+
 }
--- src/main/java/br/com/fiap/finance_walk_api/repository/TransactionRepository.java
+++ src/main/java/br/com/fiap/finance_walk_api/repository/TransactionRepository.java
@@ -1,15 +1,18 @@
 package br.com.fiap.finance_walk_api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import br.com.fiap.finance_walk_api.model.Transaction;
 
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+public interface TransactionRepository extends JpaRepository<Transaction, Long>, JpaSpecificationExecutor<Transaction> {
 
-    //List<Transaction> findByDescriptionContainingIgnoringCase(String description); //Query Method
+    // List<Transaction> findByDescriptionContainingIgnoringCase(String
+    // description); //Query Method
 
-    //List<Transaction> findByDescriptionContainingIgnoringCaseAndDate(String description, LocalDate date);
+    // List<Transaction> findByDescriptionContainingIgnoringCaseAndDate(String
+    // description, LocalDate date);
 
-    //List<Transaction> findByDate(LocalDate date);
-    
+    // List<Transaction> findByDate(LocalDate date);
+
 }
