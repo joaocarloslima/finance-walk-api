4bc6bdde80a0819d29ca08c46d0ece31acb362b1
joaocarloslima <joaocarloslima@me.com>
2025-04-30 14:40:32 -0300

    add user authentication and role management; implement user registration and password encoding

--- src/main/java/br/com/fiap/finance_walk_api/config/DatabaseSeeder.java
+++ src/main/java/br/com/fiap/finance_walk_api/config/DatabaseSeeder.java
@@ -8,12 +8,16 @@ import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import br.com.fiap.finance_walk_api.model.Category;
 import br.com.fiap.finance_walk_api.model.Transaction;
 import br.com.fiap.finance_walk_api.model.TransactionType;
+import br.com.fiap.finance_walk_api.model.User;
+import br.com.fiap.finance_walk_api.model.UserRole;
 import br.com.fiap.finance_walk_api.repository.CategoryRepository;
 import br.com.fiap.finance_walk_api.repository.TransactionRepository;
+import br.com.fiap.finance_walk_api.repository.UserRepository;
 import jakarta.annotation.PostConstruct;
 
 @Configuration
@@ -24,6 +28,12 @@ public class DatabaseSeeder {
 
     @Autowired
     private TransactionRepository transactionRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @PostConstruct
     public void init() {
@@ -66,11 +76,24 @@ public class DatabaseSeeder {
                             .date(LocalDate.now().minusDays(new Random().nextInt(30)))
                             .type(TransactionType.EXPENSE)
                             .category(categories.get(new Random().nextInt(categories.size())))
-                            .build()
-                        );
+                            .build());
         }
 
         transactionRepository.saveAll(transactions);
+
+        userRepository.saveAll(List.of(
+                User.builder()
+                        .email("joao@fiap.com.br")
+                        .password(passwordEncoder.encode("12345"))
+                        .role(UserRole.ADMIN)
+                        .build(),
+
+                User.builder()
+                        .email("maria@fiap.com.br")
+                        .password(passwordEncoder.encode("12345"))
+                        .role(UserRole.USER)
+                        .build()));
+
     }
 
 }
---
+++ src/main/java/br/com/fiap/finance_walk_api/model/UserRole.java
@@ -0,0 +1,6 @@
+package br.com.fiap.finance_walk_api.model;
+
+public enum UserRole {
+    ADMIN,
+    USER
+}
